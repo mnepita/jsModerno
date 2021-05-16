85415b0ce0e36489368662590e9f0ee8f7602ef5
Martin Nepita <35706069+mnepita@users.noreply.github.com>
2021-05-16 12:22:02 -0700

    funciones

---
+++ 09-Funciones/js/02-app.js
@@ -0,0 +1,20 @@
+/*
+Declaracion de funcion (Function declaration)
+
+- las funciones pueden ser reutilizables
+*/
+
+function sum(){
+    console.log(2+2);
+}
+
+
+sum()
+
+
+//function expression - expresion de la funcion
+const sum2 = function() {
+    console.log(3+3);
+}
+
+sum2();
