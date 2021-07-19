f304b2eee31512acc6c63be5ed04aa0af0005653
Martin Nepita <35706069+mnepita@users.noreply.github.com>
2021-07-19 08:07:04 -0700

    render cart list to html

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -30,30 +30,53 @@ function readCourseData( selectedCourse ) {
     price: selectedCourse.querySelector('.precio span').textContent,
     id: selectedCourse.querySelector('a').getAttribute('data-id'),
     quantity: 1
-
   }
 
   //add elements on the cartItems array to cart
   addToCart(courseInfo);
-
 }
-
 
 function addToCart(courseInfo) {
   cartItems = [...cartItems, courseInfo];
   htmlCartItems();
 }
 
-
-
-// renders cart items on HTML element
+// renders cart items on HTML element based on the array
 function htmlCartItems() {
+  // clean html
+  cleanHTML();
+
+  // iterates cart to generate html
   cartItems.forEach(curso => {
     const row = document.createElement('tr');
-    row.innerHTML = `
+    row.innerHTML =
+    `
+    <td>
+      <img src="${curso.image}">
+    </td>
     <td>
       ${curso.title}
     </td>
+    <td>
+      ${curso.price}
+    </td>
+     <td>
+      ${curso.quantity}
+    </td>
     `;
+    // insert html to the cart list tbody
+    contenedorCarrito.appendChild(row);
   });
+}
+
+
+// deletes tbody courses
+function cleanHTML() {
+  // // metodo lento
+  // contenedorCarrito.innerHTML = '';
+
+  while(contenedorCarrito.firstChild) {
+    contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+
+  }
 }
