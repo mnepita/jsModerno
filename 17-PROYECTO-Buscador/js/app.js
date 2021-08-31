67e632ff738b7fa1de5bf4613a6e277552a993a7
Martin Nepita <35706069+mnepita@users.noreply.github.com>
2021-08-30 18:41:45 -0700

    Update app.js

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,21 +1,64 @@
 // variables
 const result = document.querySelector('#resultado');
-const carYear = document.querySelector('#year')
+const marca = document.querySelector('#marca');
+const carYear = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
 const maxYear = new Date().getFullYear();
 const minYear = maxYear - 10;
+const searchData = {
+  marca:'',
+  year:'',
+  min: '',
+  max:'',
+  puertas: '',
+  transmision:'',
+  color:''
 
+}
 //events
 document.addEventListener('DOMContentLoaded', () => {
-  showCars(); //show card on load
+  showCars(autos); //show card on load
 
   // fill year selection
   showYearSelection();
+
+  marca.addEventListener('change', e => {
+    searchData.marca = e.target.value;
+    carFilter();
+  });
+
+  carYear.addEventListener('change', e => {
+    searchData.year = parseInt(e.target.value);
+    carFilter();
+  });
+
+  minimo.addEventListener('change', e => {
+    searchData.minimo = e.target.value;
+  });
+
+  maximo.addEventListener('change', e => {
+    searchData.maximo = e.target.value;
+  });
+
+  transmision.addEventListener('change', e => {
+    searchData.t = e.target.value;
+  });
+
+  color.addEventListener('change', e => {
+    searchData.color = e.target.value;
+  });
+
 });
 
 
 // functions
 
-function showCars() {
+function showCars(autos) {
+  clearHTML();
+
   autos.forEach(car => {
     const { marca, modelo, year, precio, puertas, transmision, color } = car;
     const carHTML = document.createElement('p');
@@ -28,6 +71,13 @@ function showCars() {
   })
 
 }
+
+function clearHTML(){
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+}
+
 function showYearSelection() {
   // console.log('year selection loaded');
   for ( let i = maxYear; i >= minYear; i-- ) {
@@ -37,4 +87,27 @@ function showYearSelection() {
     yearOption.textContent = i;
     carYear.appendChild(yearOption); //add year options
   }
+}
+
+function carFilter() {
+  const resultado = autos.filter( filtrarMarca ).filter( filtrarYear );
+  console.log(resultado);
+  showCars(resultado);
+}
+
+function filtrarMarca( auto ) {
+  const {marca} = searchData;
+  if(marca){
+    return auto.marca === marca;
+  }
+  return auto;
+}
+
+function filtrarYear( auto ) {
+  const {year} = searchData;
+  if(year){
+    return auto.year === year;
+  }
+  return auto;
+
 }
